5e4f6a5b72fd6ac98d09f79e91de450172f69e09
essssss <srmcnamara7@gmail.com>
2023-02-01 14:58:24 -0500

    done for now

--- tip-pool-test-main/helpers.test.js
+++ tip-pool-test-main/helpers.test.js
@@ -1,1 +1,11 @@
-describe('helpers test', function () {});
+describe('helpers test', function () {
+   beforeEach(function () {
+      billAmtInput.value = 100;
+      tipAmtInput.value = 20;
+   });
+
+   it('should calculate the tip', function () {
+      createCurPayment();
+      expect(createCurPayment()['tipPercent']).toEqual(20);
+   });
+});
